0f369b0ee80265da33c0c29ce6942646391f00b3
carolinapw <cpicowertag@udesa.edu.ar>
2022-11-10 08:55:42 -0300

    apis

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let url1 = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US"
+let url1 = "https://api.themoviedb.org/3/movie/popular?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US&page=1"
 
 fetch(url1)
 .then(function(res){
@@ -6,27 +6,35 @@ fetch(url1)
 })
 .then(function(data){
     console.log(data);
-    let infoPersonajes = data.results
+    let infoPeliculas = data.results
     let ofertas = document.querySelector(".container")
-    let personajes = ""
+    let todasLasPeliculas = []
 
-    for (let i = 0, i < infoPersonajes.length, i++ ) {
-        console.log(infoPersonajes[i]);
+    for (let i=0; i<6; i++) {
+        console.log(infoPeliculas[i]);
+        todasLasPeliculas += `<article>
+                                <img src="${infoPeliculas[i].poster_path} alt="${infoPeliculas[i].title}">
+                                <p><a href="detail-movie.html?id=${infoPeliculas[i].id}">Name: ${infoPeliculas[i].title}</a></p>
+                                <p>${infoPeliculas[i].release_date}
+                            </article>`
     }
+    ofertas.innerHTML = todasLasPeliculas
+    personajes.innerHTML = characters
 })
+   
 .catch(function(e){
     console.log(e)
 })
 
-let url2 = "https://api.themoviedb.org/3/tv/{tv_id}?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US"
+// let url2 = "https://api.themoviedb.org/3/movie/latest?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US"
 
-fetch(url2)
-.then(function(res){
-    return res.json();
-})
-.then(function(data){
-    console.log(data);
-})
-.catch(function(e){
-    console.log(e)
-})
+// fetch(url2)
+// .then(function(res){
+//     return res.json();
+// })
+// .then(function(data){
+//     console.log(data);
+// })
+// .catch(function(e){
+//     console.log(e)
+// })
