51818e6d9fd2b4f96bab2ef6110cf8c6539df9e8
carolinapw <cpicowertag@udesa.edu.ar>
2022-11-14 21:03:44 -0300

    titulos similares

--- js/search-results.js
+++ js/search-results.js
@@ -1,27 +1,24 @@
 let buscador = document.querySelector("#inputBusqueda")
-let query= buscador
+let query = buscador
 
-let url= `https://api.themoviedb.org/3/search/movie?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US&query=${query}C&page=1&include_adult=false`
+let urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US&query=${query}C&page=1&include_adult=false`
 
 
-fetch (url)
+fetch (urlMovie)
     .then (function(res){
         return res.json()
-})
+    })
     .then (function(data){
         console.log (data)
-        let searchMovies= data.results
-        let similares= document.querySelector("#inputBusqueda")
+        let searchMovies = data.results
+        let similares = document.querySelector("#inputBusqueda")
         let todosLosSimilares= []
 
         for (let i=0; i<searchMovies.length; i++){
-            todosLosSimilares += `<ul class="titsimilares">
-                                        <li class="listaparecidos">
-                                            <a href="./detail-serie.html">
-                                            <img class="imgtit" src="https://image.tmdb.org/t/p/w500${searchMovies[i].poster_path}"></a>
-                                            <h4 class="subtitulos descripcion" ${searchMovies[i].title}></h4>
-                                    </li>
-                                    </ul>`
+            todosLosSimilares += `<li class="listaparecidos">
+                                            <a href="./detail-serie.html?id=${searchMovies[i].id}"><img class="imgtit" src="https://image.tmdb.org/t/p/w500${searchMovies[i].poster_path}"></a>
+                                            <h4 class="subtitulos descripcion"${searchMovies[i].title}></h4>
+                                    </li>`
         }                           
 
         similares.innerHTML = todosLosSimilares
