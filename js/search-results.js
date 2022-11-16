535089f74ddc84b780c69ad2af698f05ee36ce8a
justinacademartori <mcademartori@udesa.edu.ar>
2022-11-16 17:59:39 -0300

    funciona el buscador :)

--- js/search-results.js
+++ js/search-results.js
@@ -1,14 +1,12 @@
-let buscador = document.querySelector("#inputBusqueda")
-let query = buscador.value
-
-console.log(query);
+let buscador = document.querySelector(".search")
+let queryString = location.search;
+let queryStringObj = new URLSearchParams (queryString);
+let query = queryStringObj.get("buscador");
 
 let urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US&query=${query}&page=1&include_adult=false`
 
-let queryString = location.search;
-let queryStringObj = new URLSearchParams (queryString);
 
-queryStringObj.get("search barra");
+
 
 fetch (urlMovie)
     .then (function(res){
@@ -17,13 +15,12 @@ fetch (urlMovie)
     .then (function(data){
         console.log (data)
         let searchMovies = data.results
-        let similares = document.querySelector(".titsimilares")
+        let similares = document.querySelector(".listaparecidos")
         let todosLosSimilares= []
-
         for (let i=0; i<searchMovies.length; i++){
             todosLosSimilares += `<ul class="titsimilares" id="inputBusqueda"></ul>
                                         <li class="listaparecidos">
-                                            <a href="./detail-serie.html?id=${searchMovies[i].id}"><img class="imgtit" src="https://image.tmdb.org/t/p/w500${searchMovies[i].poster_path}"></a>
+                                            <a href="./detail-movie.html?id=${searchMovies[i].id}"><img class="imgtit" src="https://image.tmdb.org/t/p/w500${searchMovies[i].poster_path}"></a>
                                             <h4 class="subtitulos descripcion"${searchMovies[i].title}></h4>
                                         </li>
                                     </ul>`
