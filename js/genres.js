013133cef63e626ea027d2b332650437d10a79a5
carolinapw <cpicowertag@udesa.edu.ar>
2022-11-19 16:35:52 -0300

    borde del article generos

--- js/genres.js
+++ js/genres.js
@@ -1,29 +1,3 @@
-let urlGenerosPeliculas = `https://api.themoviedb.org/3/genre/movie/list?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US`
-
-
-fetch (urlGenerosPeliculas)
-    .then (function(res){
-        return res.json()
-    })
-    .then (function(data){
-        console.log (data)
-        let infoPeliculas = data.genres
-        let generos = document.querySelector(".container")
-        let todasLasPeliculas = ""
-
-        for (let i=0; i<infoPeliculas.length; i++) {
-            console.log(infoPeliculas[i]);
-            todasLasPeliculas += `<article class="card">
-                                    <h3 class="subtitulos descripcion"><a href="./detail-genres.html?id=${infoPeliculas[i].id}&name=${infoPeliculas[i].name}">${infoPeliculas[i].name}</a></h3>
-                                </article>`
-        }
-        generos.innerHTML = todasLasPeliculas
-    })
-
-    .catch (function(e){
-        console.log (e)
-    })
-
 //Buscador 
 let form = document.querySelector("form")
 let buscador = document.querySelector("#inputBusqueda")
@@ -47,3 +21,29 @@ buscador.addEventListener("focus", function () {
     errorBuscador.innerText = ""
 })
 
+let urlGenerosPeliculas = `https://api.themoviedb.org/3/genre/movie/list?api_key=d7dce97c9f45ff25eeb66dc3784d0bca&language=en-US`
+
+
+fetch (urlGenerosPeliculas)
+    .then (function(res){
+        return res.json()
+    })
+    .then (function(data){
+        console.log (data)
+        let infoPeliculas = data.genres
+        let generos = document.querySelector(".container")
+        let todasLasPeliculas = ""
+
+        for (let i=0; i<infoPeliculas.length; i++) {
+            console.log(infoPeliculas[i]);
+            todasLasPeliculas += `<article class="card">
+                                    <h3 class="subtitulos descripcion"><a href="./detail-genres.html?id=${infoPeliculas[i].id}&name=${infoPeliculas[i].name}">${infoPeliculas[i].name}</a></h3>
+                                </article>`
+        }
+        generos.innerHTML = todasLasPeliculas
+
+    })
+
+    .catch (function(e){
+        console.log (e)
+    })
